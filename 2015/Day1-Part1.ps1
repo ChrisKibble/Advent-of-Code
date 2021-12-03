55d8025574f916fbef2102a69775bcd7101c47b8
Chris Kibble <chris@ckib.me>
2021-12-03 17:48:47 -0500

    Added 2015 Day 1

---
+++ 2015/Day1-Part1.ps1
@@ -0,0 +1,8 @@
+﻿$data = Get-Content $PSScriptRoot\Day1-Input.txt
+
+$up = $data.ToCharArray().Where{$_ -eq "("}.count
+$down = $data.ToCharArray().Where{$_ -eq ")"}.count
+
+$result = $up - $down
+
+$result
---
+++ 2015/Day1-Part2.ps1
@@ -0,0 +1,18 @@
+﻿$data = Get-Content $PSScriptRoot\Day1-Input.txt
+
+$position = 0
+
+For($i = 0; $i -lt $data.Length; $i++) {
+    
+    Switch($data[$i]) {
+        "(" { $position++ }
+        ")" { $position-- }
+    }
+
+    if($position -lt 0) {
+        Write-Host "Basement as Position $($i+1)"
+        break
+    }
+
+}
+
