65c4a530d117f62ebde0a19dc4df4c486214a56a
Chris Kibble <chris@ckib.me>
2021-12-03 18:37:38 -0500

    Added Day 3

---
+++ 2015/Day3-Part1.ps1
@@ -0,0 +1,22 @@
+﻿$dataIn = Get-Content $PSScriptRoot\Day3-Input.txt
+
+$board = New-Object 'Int[,]' 500,500
+
+$pos1 = 0
+$pos2 = 0
+
+$board[$pos1, $pos2]++
+
+ForEach($data in $dataIn.ToCharArray()) {
+    
+    Switch($data) {
+        "^" { $pos1++ }
+        "v" { $pos1-- }
+        "<" { $pos2-- }
+        ">" { $pos2++ }   
+    }
+    
+    $board[$pos1, $pos2]++
+}
+
+$board.where{$_ -ne 0}.count
---
+++ 2015/Day3-Part2.ps1
@@ -0,0 +1,38 @@
+﻿$dataIn = Get-Content $PSScriptRoot\Day3-Input.txt
+
+$board = New-Object 'Int[,]' 500,500
+
+$santaPos1 = 0
+$santaPos2 = 0
+
+$roboPos1 = 0
+$roboPos2 = 0
+
+$board[$santapos1, $santapos2]++
+$board[$robopos1, $robopos2]++
+
+For($i = 0; $i -lt $dataIn.Length; $i++) {
+    
+    $data = $dataIn[$i]
+
+    If($i % 2 -eq 0) {
+        Switch($data) {
+            "^" { $santaPos1++ }
+            "v" { $santaPos1-- }
+            "<" { $santaPos2-- }
+            ">" { $santaPos2++ }   
+        }
+        $board[$santaPos1, $santaPos2]++
+    } else {
+        Switch($data) {
+            "^" { $roboPos1++ }
+            "v" { $roboPos1-- }
+            "<" { $roboPos2-- }
+            ">" { $roboPos2++ }   
+        }
+        $board[$roboPos1, $roboPos2]++
+    }
+
+}
+
+Write-Host "Houses with Gifts: $($board.Where{$_ -ne 0}.count)"
