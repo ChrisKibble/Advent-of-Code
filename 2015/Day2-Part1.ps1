d6e9f81fd0821f501dc8063491f83ddba5e944fe
Chris Kibble <chris@ckib.me>
2021-12-03 18:12:17 -0500

    Completed Day 2

---
+++ 2015/Day2-Part1.ps1
@@ -0,0 +1,23 @@
+﻿$dataIn = Get-Content $PSScriptRoot\Day2-Input.txt
+
+$totalPaper = 0
+
+$rxLWH = [regex]::new("(\d{1,})x(\d{1,})x(\d{1,})")
+
+ForEach($data in $dataIn) {
+    $lwh = $rxLWH.Match($data)
+
+    [int]$length = $lwh.Groups[1].Value
+    [int]$width = $lwh.Groups[2].Value
+    [int]$height = $lwh.Groups[3].Value
+
+    [int]$panel1 = ($length * $width)
+    [int]$panel2 = ($width * $height)
+    [int]$panel3 = ($height * $length)
+
+    [int]$sqftNeeded = (2*$panel1) + (2*$panel2) + (2*$panel3) + @($panel1, $panel2, $panel3 | Measure-Object -Minimum).Minimum
+
+    [int]$totalPaper += $sqftNeeded
+}
+
+$totalPaper | Out-Host
---
+++ 2015/Day2-Part2.ps1
@@ -0,0 +1,27 @@
+﻿$dataIn = Get-Content $PSScriptRoot\Day2-Input.txt
+
+$totalRibbon = 0
+
+$rxLWH = [regex]::new("(\d{1,})x(\d{1,})x(\d{1,})")
+
+ForEach($data in $dataIn) {
+
+    [int]$ribbonLen = 0
+
+    $lwh = $rxLWH.Match($data)
+
+    [int]$length = $lwh.Groups[1].Value
+    [int]$width = $lwh.Groups[2].Value
+    [int]$height = $lwh.Groups[3].Value
+
+    [int[]]$allSides = @($length, $length, $width, $width, $height, $height)
+
+    $allSides | Sort | Select -First 4 | % { $ribbonLen += $_ }
+    
+    $ribbonLen += ($length * $width * $height)
+
+    $totalRibbon += $ribbonLen
+
+}
+
+$totalRibbon
