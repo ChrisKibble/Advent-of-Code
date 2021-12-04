173a4789364aa45a62888a1eb151c44b4ec3f1a7
Chris Kibble <chris@ckib.me>
2021-12-04 02:26:22 -0500

    Added 2015 Day 4

---
+++ 2015/Day4-Part1.ps1
@@ -0,0 +1,22 @@
+﻿$data = "yzbqklnj"
+
+Function Get-MD5Hash {
+    
+    Param (
+        [string]$data
+    )
+
+    $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+    $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($data)))
+
+    $hash -replace "-",""
+}
+
+For($i = 0; $i -lt [int]::MaxValue; $i++) {
+    $hash = Get-MD5Hash -data "$data$i"
+    if($hash.Substring(0,5) -eq "00000") {
+        Write-Host $i
+        break
+    }
+}
---
+++ 2015/Day4-Part2.ps1
@@ -0,0 +1,24 @@
+﻿$data = "yzbqklnj"
+
+Function Get-MD5Hash {
+    
+    Param (
+        [string]$data
+    )
+
+    $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+    $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($data)))
+
+    $hash -replace "-",""
+}
+
+$fiveZerosFromPart1 = 282749
+
+For($i = $fiveZerosFromPart1; $i -lt [int]::MaxValue; $i++) {
+    $hash = Get-MD5Hash -data "$data$i"
+    if($hash.Substring(0,6) -eq "000000") {
+        Write-Host $i
+        break
+    }
+}
