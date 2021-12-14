7a8af6721a3aaef36bd3c6f9bd62109589c05a38
Chris Kibble <chris@ckib.me>
2021-12-14 15:47:04 -0500

    Day 5 of 2015 completed.

---
+++ 2015/Day5-part1.ps1
@@ -0,0 +1,35 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = @"
+ugknbfddgicrmopn
+aaa
+jchzalrnumimnmhp
+haegwjzuvuyypxyu
+dvszwmarrgswjxmb
+"@ -split "`r`n"
+
+$dataIn = Get-Content $PSScriptRoot\Day5-Input.txt
+
+$nice = 0
+
+$vowels = [RegEx]::new("(?i)[aeiou]")
+$dblLetter = [RegEx]::New("(.)\1+")
+$badGroups = [RegEx]::New("(?i)(ab|cd|pq|xy)")
+
+$dataIn.ForEach{
+
+    [string]$s = $_
+
+    If($vowels.Matches($s).count -ge 3 -and $dblLetter.Matches($s).Count -ge 1 -and $badGroups.Matches($s).Count -eq 0) {
+        # Write-Host "NICE!"
+        $nice++
+    } else {
+        # Write-Host "NAUGHTY!"
+    }
+
+
+}
+
+Write-Host "There are $nice nice strings."
---
+++ 2015/Day5-part2.ps1
@@ -0,0 +1,29 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = @"
+qjhvhtzxzqqjkmpb
+xxyxx
+uurcxstgmygtbstg
+ieodomkazucvgmuy
+"@ -split "`r`n"
+
+$dataIn = Get-Content $PSScriptRoot\Day5-Input.txt
+
+$nice = 0
+
+$dblLetter = [RegEx]::New("([a-z][a-z]).*\1")
+$skipLetter = [RegEx]::New("([a-z]).\1")
+
+$dataIn.ForEach{
+
+    [string]$s = $_
+
+    If($dblLetter.Matches($s).Count -gt 0 -and $skipLetter.Matches($s).Count -gt 0) {
+        $nice++
+    }
+
+}
+
+Write-Host "There are $nice nice strings."
