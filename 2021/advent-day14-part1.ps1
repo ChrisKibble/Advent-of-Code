3874b62094378b24f0a97bc2829d12e3fc414623
Chris Kibble <chris@ckib.me>
2021-12-14 15:47:34 -0500

    2021 Day 14 done.

---
+++ 2021/advent-day14-part1.ps1
@@ -0,0 +1,60 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = @"
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+"@ -split "`r`n"
+
+$dataIn = Get-Content $PSScriptRoot\Day14-Input.txt
+
+$template = $dataIn[0]
+$ruleList = $dataIn | Select -Skip 2 | ForEach { 
+    $str = $_.Substring(0,2)
+    $add = $_.Substring(6)
+    $newStr = "$($_.Substring(0,1))$add"
+    
+    @{$str = $newStr}
+}
+
+$rxTwoChar = [RegEx]::new("(?i)(?=([A-Z][A-Z]))")
+
+1..10 | % {
+
+    $finds = $rxTwoChar.Matches($template)
+
+    $newTemplate = $finds.ForEach{
+        $ruleList.$($_.Groups[1].Value)
+    }
+
+    $newTemplate += $template[-1]
+    $template = $newTemplate -join ""
+}
+
+Write-Host "Template Length: $($template.Length)"
+
+$grp = $template.ToCharArray() | Group-Object | Select Name,Count | Sort-Object Count -Descending
+
+$maxCount = $grp[0].Count
+$minCount = $grp[-1].Count
+
+$diff = $maxCount - $minCount
+
+Write-Host "Difference from Min to Max is $diff"
---
+++ 2021/advent-day14-part2.ps1
@@ -0,0 +1,99 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = @"
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+"@ -split "`r`n"
+
+$iterationCount = 40
+
+$dataIn = Get-Content $PSScriptRoot\Day14-Input.txt
+
+$template = $($dataIn[0])
+
+$ruleList = $dataIn | Select -Skip 2 | ForEach { 
+    $str = $_.Substring(0,2)
+    
+    $add = $_.Substring(6)
+    $newStr = "$($_.Substring(0,1))$add"
+    
+    $child1 = "$($_.Substring(0,1))$add"
+    $child2 = "$add$($_.Substring(1,1))"
+
+    @{
+        $str = @($child1, $child2)
+    }
+}
+
+$rxPairs = [RegEx]::new("(?i)(?=([A-Z][A-Z]))")
+$pairs = $rxPairs.Matches($template)
+
+$map = [Ordered]@{}
+$pairs.ForEach{
+    $thisPair = $_.Groups[1].Value
+    # $map.Add($thisPair,1)
+    [Void][UInt64]$map.$thisPair++
+}
+
+$letters = @{}
+
+# Last letter ends up missing because of the way that pairs break up, start by adding it right in.
+$letters.$([string]$template[-1]) = 1
+
+
+For($i = 1; $i -le $iterationCount; $i++) {
+
+    # Write-Host "---- Iteration #$i -----"
+       
+    $newMap = @{}
+
+    $map.GetEnumerator().ForEach{     
+        
+        $str = $_.Name
+        $count = $_.Value
+        $child1 = $ruleList.$str[0]
+        $child2 = $ruleList.$str[1]
+        
+        # Write-Host "$str spawns $child1 and $child2 (#$j)"
+        [UInt64]$newMap.$child1 += $count
+        [UInt64]$newMap.$child2 += $count
+
+    }
+
+    $map = $newMap.Clone()
+  
+}
+
+
+$map.GetEnumerator().ForEach{
+    $ltr = $_.Name.Substring(0,1)
+    $letters.$ltr += $_.Value
+}
+
+[UInt64]$max = 0
+[UInt64]$min = [UInt64]::MaxValue
+
+$letters.GetEnumerator().ForEach{
+    $min = [Math]::Min([UInt64]$min, [Uint64]$_.Value)
+    $max = [Math]::Max([UInt64]$max, [Uint64]$_.Value)
+}
+
+Write-Host "Diff = $($max-$min)"
