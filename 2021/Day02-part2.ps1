5ada51e0e3f907aba4482e2b21af573042dda0ce
Chris Kibble <chris@ckib.me>
2021-12-15 11:47:34 -0500

    Renamed for better sorting.

---
+++ 2021/Day02-part2.ps1
@@ -0,0 +1,24 @@
+﻿$input = Get-Content C:\temp\input.txt
+
+$horizontal = 0
+$depth = 0 
+$aim = 0
+
+ForEach($cmd in $input) {
+    
+    $cmdName = $cmd.substring(0,$cmd.indexof(' '))
+    $cmdNum = [int]$cmd.Substring($cmd.indexof(' ')+1)
+
+    Switch($cmdName) {
+        "forward" { 
+            $horizontal += $cmdNum
+            $depth += ($aim * $cmdNum)
+        }
+        "up" { $aim -= $cmdNum }
+        "down" { $aim += $cmdNum }
+        default { throw $cmdName }
+    }
+
+}
+
+$horizontal * $depth
