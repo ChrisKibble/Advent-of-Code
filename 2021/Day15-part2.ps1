6082b78058010b0710f19327965d556c029200d0
Chris Kibble <chris@ckib.me>
2021-12-17 10:58:52 -0500

    Added Day 15 P2, Day16 P1, and Started D16 P2

---
+++ 2021/Day15-part2.ps1
@@ -0,0 +1,118 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = @"
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+"@ -split "`r`n"
+
+$dataIn = Get-Content $PSScriptRoot\Day15-Input.txt
+
+#5x to the right
+
+$map = @()
+For($rowNumber = 0; $rowNumber -lt $dataIn.length; $rowNumber++) {
+    $thisRow = $dataIn[$rowNumber]
+    [string]$newRow = $thisRow
+    For($i = 1; $i -lt 5; $i++) {
+        $thisRow.ToCharArray().ForEach{
+            [Int]$newInt = [String]$_ -as [int]
+            $newInt += $i
+            If($newInt -gt 9) { $newInt = $newInt - 9 }
+            $newRow += [string]$newInt
+        }
+    }
+    $map += $newRow
+}
+
+$dataIn = $map
+
+$newRows = @()
+For($i = 1; $i -lt 5; $i++) {
+    For($rowNumber = 0; $rowNumber -lt $dataIn.Length; $rowNumber++) {
+        $thisRow = $dataIn[$rowNumber]
+        [string]$newRow = ""
+        
+        $thisRow.ToCharArray().ForEach{
+            [Int]$newInt = [String]$_ -as [int]
+            $newInt += $i
+            If($newInt -gt 9) { $newInt = $newInt - 9 }
+            $newRow += [string]$newInt
+        }
+        
+        $newRows += $newRow
+    }
+}
+
+$dataIn = $map + $newRows
+
+$points = @{}
+$visited = @{}
+
+For($rowNumber = 0; $rowNumber -lt $dataIn.length; $rowNumber++) {
+    For($charNumber = 0; $charNumber -lt $dataIn[0].length; $charNumber++) {
+        $coord = "$rowNumber`x$charNumber"
+        $points.$coord = [Int]::MaxValue
+        $visited.$coord = 0
+    }
+}
+
+$points.'0x0' = 0
+
+$pointQueue = [System.Collections.Generic.PriorityQueue[object, int]]::new()
+$pointQueue.Enqueue("0x0", 0)
+
+Write-Host "Starting Queue Parsing"
+
+While($pointQueue.Count -gt 0) {
+
+    # Get my next point
+    $currentPoint = $pointQueue.Dequeue()
+
+    If($visited.$currentPoint -eq 1) {
+        continue
+    }
+
+    $startDistance = $points.$currentPoint
+   
+    $x = [Int]$currentPoint.substring(0, $currentPoint.IndexOf('x'))
+    $y = [Int]$currentPoint.substring($currentPoint.IndexOf('x') + 1)
+
+    $neighbors = @()
+    If($x -gt 0) { $neighbors += "$($x-1)x$y" } # Neighbor Left
+    If($y -gt 0) { $neighbors += "$x`x$($y-1)" } # Neighbor Up
+
+    If($x -lt $dataIn[0].length-1) { $neighbors += "$($x+1)x$y" } # Neighbor Right
+    If($y -lt $dataIn.length-1) { $neighbors += "$x`x$($y+1)" } # Neighbor Left
+
+    # Write-Host "I am at $currentPoint ($x,$y). My cost is $startDistance."
+
+    ForEach($neighbor in $neighbors) {
+        If($points.$neighbor) {
+            $nx = [Int]$neighbor.substring(0,$neighbor.IndexOf("x"))
+            $ny = [Int]$neighbor.substring($neighbor.IndexOf("x")+1)
+            $nVal = [String]$dataIn[$nx][$ny] -as [int]
+            $nValTotal = $nVal + $startDistance
+            # Write-Host "  My neighbor at $neighbor ($nx,$ny) has a distance of $($points.$neighbor)."
+            # Write-Host "  The real distance for my neighbor is $nVal (for a total distance from 0x0 of $nValTotal)"
+            $neighborCost = [Math]::Min($nValTotal, $points.$neighbor)
+            $points.$neighbor = $neighborCost
+            $pointQueue.Enqueue($neighbor, $neighborCost)
+        }
+    }
+
+    $visited.$currentPoint = 1
+}
+
+$lastPos = "$($dataIn[0].length-1)x$($dataIn.length-1)"
+
+Write-Host "Shortest Path = $($points.$lastPos)"
---
+++ 2021/Day16-part2.ps1
@@ -0,0 +1,118 @@
+﻿Clear-Host
+
+Get-Variable | Remove-Variable -ErrorAction SilentlyContinue
+
+$dataIn = "D2FE28"
+
+
+$dataIn = "C200B40A82"
+
+
+$bitList = [System.Collections.ArrayList]::New(@())
+
+$dataIn.ToCharArray().ForEach{
+
+    $str = [string]$_
+    $dec = [uint32]"0x$str"
+    $bin = $([Convert]::ToString($dec,2)).PadLeft(4,'0')
+    
+    [Void]$bitList.Add(
+        [PSCustomObject]@{
+            "Hex" = $str
+            "Decimal" = $dec
+            "Binary" = $bin
+        }
+    )
+
+}
+
+$bitString = $bitList.Binary -join ""
+$action = $null
+
+[Int64]$total = 0
+[Int]$VersionSum = 0
+
+While($bitString.Length -ge 4) {
+
+    #Write-Host "Bitstring is now $bitString with length $($bitString.Length)" 
+
+    Switch($action) {
+    
+        $null {
+
+            If($bitString.Length -lt 10) { 
+                # Nothing we can do with less than 10 characters if we're not in an action alrady
+                return
+            }
+
+
+            Write-Host "Not sure what to do, so time to read in the version and type bits."
+            $packetVersion = $bitString.Substring(0,3)
+            $packetType = $bitString.Substring(3,3)
+            $packetVersion = [Convert]::ToInt32($packetVersion, 2)
+            $packetType = [Convert]::ToInt32($packetType, 2)
+            $bitString = $bitString.Substring(6)
+            Write-Host "      Version $packetVersion"
+            Write-Host "      Type $packetType"
+            
+            $VersionSum += $packetVersion
+
+            If($packetType -eq "4") {
+                Write-Host "Switching to Literal Mode!"
+                $action = "L"
+                [String]$literal = ""
+            } else {
+                Write-Host "Switching to Operator Mode"
+                If($bitString.Substring(0,1) -eq 0) {
+                    $opBitLen = 15
+                } else {
+                    $opBitLen = 11
+                }
+
+                $bitString = $bitString.Substring(1)
+
+                Write-Host "   Bit Length = $opBitLen"
+
+                $subpacketLength = [Convert]::ToInt32($bitString.Substring(0, $opBitLen), 2)
+                Write-Host "   Subpacket Length is $($bitString.Substring(0, $opBitLen)) ($subpacketLength)"
+
+                $bitString = $bitString.Substring($opBitLen)
+                $message = $bitString.Substring(0, $subpacketLength)
+                
+                Write-Host "   Message is $message"
+                
+            }
+
+
+        }
+
+        "L" {
+            
+            Write-Host "   I'm working a literal packet. Grabbing next five bits."
+            
+            $nextBits = $bitString.Substring(0,5)
+            
+            $bitString = $bitString.Substring(5)
+            $litBin = $nextBits.Substring(1,4)
+
+            $literal += $litBin
+            
+            If($nextBits.substring(0,1) -eq "0") {
+                
+                Try {
+                    $literalNum = $([Convert]::ToInt64($literal, 2))
+                } Catch {
+                    Throw $_
+                }
+                Write-Host "     This was be my last literal capture."
+                Write-Host "     Value: $literal ($literalNum)"
+                $total = $total + $literalNum
+                $action = $null
+            }
+
+
+        }
+    
+    }
+
+}
