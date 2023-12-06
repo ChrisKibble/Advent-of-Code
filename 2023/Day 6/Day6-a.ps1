5dcf8e0f764d096b7d7b24a8a8ebdd5ebab6514a
Christopher Kibble <christopher.kibble@biogen.com>
2023-12-06 09:58:59 -0500

    Day 6 Part 1 Complete

---
+++ 2023/Day 6/Day6-a.ps1
@@ -0,0 +1,75 @@
+Clear-Host
+Get-Date
+Write-Host "---"
+
+Function Get-RecordWinners {
+
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [Int]$TimeMS,
+
+        [Parameter(Mandatory=$True)]
+        [Int]$RecordDistance
+    )
+
+    Write-Verbose "Need to find how long to hold the button to beat $RecordDistance`mm in a race length of $TimeMS`ms"
+    $topOfCurve = [Math]::Round($TimeMS/2)
+
+    Write-Verbose "The top of the curve is $topOfCurve"
+
+    If($TimeMS % 2 -eq 0) {
+        $TimeIsEven = $True
+    } Else {
+        $TimeIsEven = $False
+    }
+
+    $nextPotentialWinner = [Math]::Round($topOfCurve)
+
+    [System.Collections.Generic.List[Int]]$RecordWinners = Do {
+        $RunTime = $TimeMS - $nextPotentialWinner
+        $Distance = $RunTime * $nextPotentialWinner
+        $IsRecordWinner = $Distance -gt $RecordDistance
+        
+        Write-Verbose "Testing $NextPotentialWinner"
+        Write-Verbose "  Button Hold Time = $NextPotentialWinner"
+        Write-Verbose "  RunTime = $RunTime"
+        Write-Verbose "  Distance (Calc) = $distance"
+        Write-Verbose "  RecordWinner = $isRecordWinner"
+        
+        If($IsRecordWinner) { $NextPotentialWinner }
+
+        $nextPotentialWinner++
+    } Until(-Not($IsRecordWinner))
+
+    Write-Verbose "Found $($RecordWinners.Count) Record Winners in first pass."
+    If($TimeIsEven) {
+        Write-Verbose "I am Even"
+        $TotalWinners = ($RecordWinners.Count - 1) * 2 + 1
+    } Else {
+        Write-Verbose "I am Odd"
+        $Totalwinners = ($RecordWinners.Count - 1) * 2
+    }
+
+    Write-Verbose "Total Winners = $TotalWinners"
+
+    $TotalWinners
+
+}
+
+
+$data = Get-Content $PSScriptRoot\Input.txt
+$times = $data[0] -split '\s+' | Select-Object -Skip 1
+$distances = $data[1] -split '\s+' | Select-Object -Skip 1
+
+$WinCounts = For($run = 0; $run -lt $times.Count; $run++) {
+    $thisWin = Get-RecordWinners -TimeMS $times[$run] -RecordDistance $distances[$run]
+    $thisWin
+}
+
+$product = 1
+$WinCounts.ForEach{
+    $product = $product * $_
+}
+
+$Product
