3473e358cee50b86adfa9f24218e426b3fd77646
Christopher Kibble <christopher.kibble@biogen.com>
2024-11-26 16:05:15 -0500

    Day 10 complete.

---
+++ 2015/Day10-Part2.ps1
@@ -0,0 +1,36 @@
+Function Get-LookAndSay {
+    [CmdLetBinding()]
+    Param(
+        [String]$Data
+    )
+
+    Write-Verbose "Reading: $Data"
+
+    $x = Measure-Command {
+        $contents = [RegEx]::Matches($data,'(0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)').Value
+    }
+    
+    Write-Verbose "RegEx Took $($x.TotalMilliseconds)ms and returned $($contents.Count) blocks"
+
+    $x = Measure-Command {
+        [System.Collections.Generic.List[String]]$Output = @()
+        ForEach($block in $contents) {
+            $len = $block.Length
+            $char = $block[0]
+            $Output.Add("$len$char")
+        }
+    }
+    
+    Write-Verbose "Loop took $($x.TotalMilliseconds)ms and returned length $($output.Count)."
+
+    $out = $output -join ''
+    Write-Verbose "Writing: $out"
+    Return $out
+}
+
+1..50 | % {
+    $_
+    $data = Get-LookAndSay -Data (Get-Content $PSScriptRoot\Day10-Input.txt)
+}
+
+$data.Length
