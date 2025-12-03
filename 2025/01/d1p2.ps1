758d6334f4a970b0ad08d2cacedf713acd2dfb5d
Christopher Kibble <christopher.kibble@biogen.com>
2025-12-03 16:09:45 -0500

    Completed Day 1 Part 2

---
+++ 2025/01/d1p2.ps1
@@ -0,0 +1,41 @@
+[CmdLetBinding()]Param()
+
+# $instructions = Get-Content $PSScriptRoot\sample.txt
+$instructions = Get-Content $PSScriptRoot\input.txt
+
+$dialPosition = 50
+$zeroCount = 0
+
+Write-Verbose "- The dial starts by pointing at $dialPosition"
+ForEach($move in $instructions) {
+    
+    $lastPosition = $dialPosition    
+
+    $direction = $move.substring(0,1)
+    $notDirection = $move.substring(1)
+        
+    [Int]$change = $notDirection % 100
+  
+    $thisZeroCount = [Math]::Floor($notDirection/100)
+
+    If($direction -eq "L") { $Change = $Change * -1 }
+    $dialPosition += $Change
+
+    If($dialPosition -lt 0) {
+        $dialPosition = 100 + $dialPosition
+        If($lastPosition -ne 0) {
+            $thisZeroCount++
+        }
+    } ElseIf($dialPosition -gt 99) {
+        $dialPosition = $dialPosition - 100
+        $thisZeroCount++
+    } ElseIf($dialPosition -eq 0) {
+        $thisZeroCount++
+    }
+
+    $zeroCount += $thisZeroCount
+
+    Write-Verbose "- The dial is rotated $move to point at $dialPosition; during this rotation, it points at zero $thisZeroCount time(s)."
+}
+
+Write-Output "The dial hit position zero $zeroCount times."
